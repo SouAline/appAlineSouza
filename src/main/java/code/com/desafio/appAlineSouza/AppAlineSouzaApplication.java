27e3be818486d94717217d6d8a25fe14c096e547
Aline Souza <ds.alinesouza@gmail.com>
2021-08-05 21:30:51 -0300

    Projeto iniciado

---
+++ src/main/java/code/com/desafio/appAlineSouza/AppAlineSouzaApplication.java
@@ -0,0 +1,13 @@
+package code.com.desafio.appAlineSouza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAlineSouzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAlineSouzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/code/com/desafio/appAlineSouza/AppAlineSouzaApplicationTests.java
@@ -0,0 +1,13 @@
+package code.com.desafio.appAlineSouza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAlineSouzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
