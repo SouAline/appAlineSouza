3702534233800cbd350a1320f6abdc32c28c834e
Aline Souza <ds.alinesouza@gmail.com>
2021-08-11 20:15:55 -0300

    cria atributos

--- src/main/java/code/com/desafio/appAlineSouza/model/domain/Evento.java
+++ src/main/java/code/com/desafio/appAlineSouza/model/domain/Evento.java
@@ -1,5 +1,10 @@
 package code.com.desafio.appAlineSouza.model.domain;
 
 public class Evento {
+	private String nome;
+	private String local;
+	private String cidade;
+	private float valorIngresso;
+	private boolean temAtracoes;
 
 }
