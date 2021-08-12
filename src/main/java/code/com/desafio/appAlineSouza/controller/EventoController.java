7790ae8c1cd9572d78bf116478a5695d60da121d
Aline Souza <ds.alinesouza@gmail.com>
2021-08-11 23:12:53 -0300

    Cria rotas na classe EventoController

--- src/main/java/code/com/desafio/appAlineSouza/controller/EventoController.java
+++ src/main/java/code/com/desafio/appAlineSouza/controller/EventoController.java
@@ -1,17 +1,22 @@
 package code.com.desafio.appAlineSouza.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EventoController {
 	
+	
+	
+	@GetMapping(value = "/")
 	public String inicializa() {
-		return "";
+		return "index";
 		
 	}
 	
+	@GetMapping(value = "/eventos/agendaLista")
 	public String obterLista() {
-		return"";
+		return"eventos/agendaLista";
 	}
 	
 
