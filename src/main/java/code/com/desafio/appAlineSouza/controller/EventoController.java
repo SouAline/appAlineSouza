5e734e427f5b628712afb7004fcca236adb023ce
Aline Souza <ds.alinesouza@gmail.com>
2021-08-11 23:02:12 -0300

    cria classe EventoController

---
+++ src/main/java/code/com/desafio/appAlineSouza/controller/EventoController.java
@@ -0,0 +1,18 @@
+package code.com.desafio.appAlineSouza.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EventoController {
+	
+	public String inicializa() {
+		return "";
+		
+	}
+	
+	public String obterLista() {
+		return"";
+	}
+	
+
+}
