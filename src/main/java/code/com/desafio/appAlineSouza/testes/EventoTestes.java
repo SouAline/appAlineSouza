704a6f946fee3d203d66854dcbf319e44b95a6f3
Aline Souza <ds.alinesouza@gmail.com>
2021-08-11 23:03:01 -0300

    Cria classe EventoTest

---
+++ src/main/java/code/com/desafio/appAlineSouza/testes/EventoTestes.java
@@ -0,0 +1,15 @@
+package code.com.desafio.appAlineSouza.testes;
+
+import code.com.desafio.appAlineSouza.model.domain.Evento;
+
+public class EventoTestes {
+
+	public static void main(String[] args) {
+		Evento evento = new Evento();
+		evento.setNome("Palestra Auto Ajuda");
+		evento.setCidade("Cachoeirinha");
+		evento.setValorIngresso(15);
+
+	}
+
+}
