d81f2b12ffdbf48b8c887d0777893741fc878266
Aline Souza <ds.alinesouza@gmail.com>
2021-08-14 20:02:44 -0300

    atualiza classe alterando as rotas

--- src/main/java/code/com/desafio/appAlineSouza/controller/EventoController.java
+++ src/main/java/code/com/desafio/appAlineSouza/controller/EventoController.java
@@ -1,22 +1,30 @@
 package code.com.desafio.appAlineSouza.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EventoController {
-	
-	
-	
-	@GetMapping(value = "/")
+		
+	@GetMapping(value = "/")//rotas que são chamadas
 	public String inicializa() {
-		return "index";
+		return "index";//telas que são mostradas
 		
 	}
 	
 	@GetMapping(value = "/eventos/agendaLista")
-	public String obterLista() {
-		return"eventos/agendaLista";
+	public String obterLista(Model model) {
+		List<String> colecaoEventos = new ArrayList<String>();
+		colecaoEventos.add("Show Annita");
+		colecaoEventos.add("Show A");
+	
+		model.addAttribute("eventos", colecaoEventos);
+		
+		return "eventos/agendaLista";
 	}
 	
 
