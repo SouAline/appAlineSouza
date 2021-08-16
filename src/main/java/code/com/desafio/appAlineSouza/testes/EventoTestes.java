31b0668e850bdbda998752e3d2c4880af5d3558e
Aline Souza <ds.alinesouza@gmail.com>
2021-08-15 22:20:01 -0300

    Cria evento na classe de teste

--- src/main/java/code/com/desafio/appAlineSouza/testes/EventoTestes.java
+++ src/main/java/code/com/desafio/appAlineSouza/testes/EventoTestes.java
@@ -5,11 +5,9 @@ import code.com.desafio.appAlineSouza.model.domain.Evento;
 public class EventoTestes {
 
 	public static void main(String[] args) {
-		Evento evento = new Evento();
-		evento.setNome("Palestra Auto Ajuda");
-		evento.setCidade("Cachoeirinha");
-		evento.setValorIngresso(15);
-		System.out.print("O evento custa R$: " + evento);
+		Evento evento = new Evento("Show Anitta", 20);
+		
+		System.out.print("Evento: " + evento);
 
 	}
 
